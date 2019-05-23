97a0fbf8d11772208c33c0c88ae4b8d61b5507a6
Scott Thomas <smthom05@gmail.com>
2019-05-23 10:16:58 -0600

    Change test to just call the GET and check instances

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,16 @@
 import { expect } from "chai"
 import sinon from "sinon"
+import $ from 'jquery'
+
 
 describe("Word watch functions", () => {
   describe("As a visitor", () => {
     describe("when I visit the index page", () => {
       it("should show me the top word and its count on the right hand side", () => {
-        word = document.getElementById('top_word')
-        word_freq = document.getElementById('top_word_freq')
-        expect(word).to.be.an.instanceOf(String)
-        expect(word_freq).to.be.an.instanceOf(Integer)
+        $.get('https://wordwatch-api.herokuapp.com/api/v1/top_word', function( data ){
+          expect(Object.keys(data.word)[0]).to.be.an.instanceOf(String)
+          expect(Object.values(data.word)[0]).to.be.an.instanceOf(Integer)
+        })
       })
     })
   })
