2b1533d2caf01ef78a006a3dbb016ef4c607e7a8
Scott Thomas <smthom05@gmail.com>
2019-05-23 09:20:53 -0600

    Add basic test for seeing most popular word

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -2,9 +2,12 @@ import { expect } from "chai"
 import sinon from "sinon"
 
 describe("Word watch functions", () => {
-  describe("", () => {
-    it("", () => {
+  describe("As a visitor", () => {
+    describe("when I visit the index page", () => {
+      it("should show me the top word and its count on the right hand side", () => {
+        expect(page).to have_content('check')
+          .and have_content(497)
+      })
     })
   })
 })
-
